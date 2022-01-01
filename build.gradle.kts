8f4a88c129ee8e25c1e40fd7a93d5a9555f5cf04
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2022-01-01 14:07:17 -0500

    publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `kotlin-dsl`
+    `maven-publish`
 }
 
 group = "skytils"
@@ -25,4 +26,16 @@ gradlePlugin.plugins.create("KnockOffMixinGradle") {
     implementationClass = "skytils.gradle.plugin.knockoffmixingradle"
     displayName = "KnockOffMixinGradle"
     description = "Please send help"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.Skytils"
+            artifactId = "KnockOffMixinGradle"
+            version = "LOCAL"
+
+            from(components["java"])
+        }
+    }
 }
